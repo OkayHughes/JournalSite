909076b5bee631f0ae1c38680b451d5d41c3f48e
OkayHughes <OkayHughes@gmail.com>
2016-01-04 19:34:41 -0500

    asdf

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -94,7 +94,8 @@ class EntriesController < ApplicationController
         end
       end
     end
-    @entries = query1.results | query2.results
+
+    @entries = query1.results << query2.results.count
     @subjects = []
     @entries.each do |entry|
       entry_subjects = entry.subjects.split(",")
